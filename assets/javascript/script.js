b7f3432a9673411013c9e5272a24d5e63c2ffc35
MRusinova <rusinovamihaela@gmail.com>
2022-10-25 10:35:33 +0000

    Add function to hide previous question

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,10 @@
 // displaying the first block of questions
 
 document.getElementsByClassName("container")[0].style.display = "block";
+
+// Defining the next function
+
+function next(id) {
+    document.getElementsByClassName("container")[id-1].style.display = "none";
+    document.getElementsByClassName("container")[id].style.display = "block";
+}
